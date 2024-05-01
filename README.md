8a06616ac2e8771f9141b1f6f44d09617d418e06
Florian Beaumont <25250981+BFlorian91@users.noreply.github.com>
2024-05-01 13:44:19 +0200

    fish config: v2

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# fish_config
+# 🐠 Fish Shell Configuration README
+
+This guide provides a simple and concise understanding of this Fish Shell configuration. 
+
+## 🚀 Shortcuts
+
+- `de`: This launches a Docker development environment.
+- `npm` and `p`: These use `pnpm` instead of `npm`.
+- `pu`: This updates `pnpm`.
+- `prd`: This runs the `pnpm run dev` command.
+- `prt`: This runs the `pnpm run test` command.
+- `prtc`: This runs the `pnpm run test:coverage` command.
+- `rtj` and `vt`: These execute specific scripts.
+- `nn` and `n`: These open Neovim in the current directory.
+- `nc`: This opens Neovim configuration.
+- `rl`: This reloads the terminal.
+- `config`: This opens Fish Shell configuration in Neovim.
+- `suo`: This sets the current branch to track the origin branch.
+- `act`: This runs GitHub Actions locally with a specific architecture.
+- `bt`: This executes a shortcut to turn on the desktop light.
+- `serv`: This launches a BrowserSync server.
+- `gcb`, `gco`, `gcm`, `gcl`, `gpl`, `gps`: These are shortcuts for various Git commands.
+
+## 📂 Paths
+
+- The path to the Flutter project is added to `PATH`.
+- The path to `pnpm` is added to `PATH`.
+- The path to the `bin` directory is added to `PATH`.
+
+## 🎛️ Configuration
+
+- Vi key bindings are being used.
+- There is a `new` function to clone a specific Git project.
+- `bun` and `pnpm` have been configured.
+- Custom key bindings for Vi have been set up.
+
+Please note that this configuration may require adjustments based on specific needs. Happy coding! 🚀👩‍💻👨‍💻
---
+++ config.fish
@@ -0,0 +1,68 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  alias de="cd /Users/florian/Documents/dockerDevEnvironment && docker-compose up -d && docker-compose exec neovim fish"
+  alias npm="pnpm"
+  alias p="pnpm"
+  alias pu="pnpm add -g pnpm" # pnpm update
+  alias prd="pnpm run dev"
+  alias prt="pnpm run test"
+  alias prtc="pnpm run test:coverage"
+  alias rtj="~/Documents/Development/scripts/rtj.sh"
+  alias vt="~/Documents/Development/scripts/vtj.sh"
+  alias nn="nvim ."
+  alias n="nvim"
+  alias nc="nvim ~/.config/nvim/"
+  alias rl="source ~/.config/fish/config.fish; and clear; and echo '⚙️  Terminal reloaded ✅'"
+  alias config="nvim ~/.config/fish/"
+  alias suo="--set-upstream origin"
+  alias act="act --container-architecture linux/arm64"
+	alias bt="shortcuts run 'Turn the Desktop Light'"
+	alias serv="browser-sync start --server --files \"*.html, css/*.css\""
+	alias gcb="git checkout -b"
+	alias gco="git checkout"
+	alias gcm="git commit -m"
+	alias gcl="git clone"
+	alias gpl="git pull"
+	alias gps="git push"
+  
+end
+
+set PATH ~/Documents/Development/sandbox/flutter/core/ $PATH
+set -o vi
+function fish_user_key_bindings
+
+end
+
+# Crée un alias pour cloner un projet Git à partir de GitHub
+function new
+  if test -z $argv
+    echo "Usage: new YOUR_PROJECT_NAME"
+  else
+    git clone git@github.com:FlorianBx/react-typescript-starter.git $argv
+  end
+end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME /Users/florian/Library/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# bin directory
+set -gx PATH "$HOME/bin" $PATH
+
+# bin directory end
+fish_vi_key_bindings
+
+function fish_user_key_bindings
+  fish_vi_key_bindings
+  bind -M insert -m default jk backward-char force-repaint
+	bind -M insert \t accept-autosuggestion
+end
+
+set fish_key_bindings fish_user_key_bindings
